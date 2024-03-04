1a61e616ed0b3138388e7565d95b59325d996ccd
SPtraining <160187846+SPtraining@users.noreply.github.com>
2024-03-04 14:59:55 -0500

    Update test.tf

--- test.tf
+++ test.tf
@@ -13,11 +13,11 @@ terraform {
 
 
 provider "aws" {
-  region     = "us-east-2"
+  region     = "us-east-2b"
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-09988af04120b3591"
+   ami = "ami-02ca28e7c7b8f8be1"
    instance_type = "t2.micro"
    key_name = "linuxt2medium"
    tags = {
